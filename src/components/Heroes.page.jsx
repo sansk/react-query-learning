7483243a3cebfe765f95f7133038a95374e53c5f
sansk <iamsangyk@gmail.com>
2023-06-07 19:02:53 +0530

    using normal useState & useEffect to fetch data

--- src/components/Heroes.page.jsx
+++ src/components/Heroes.page.jsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const url = "http://localhost:4000/heroes";
+
 export const HeroesPage = () => {
-  return <>Heroes Page</>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const resp = await axios.get(url);
+    console.log(resp.data);
+    setData(resp.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <section>
+        {data.map((item) => {
+          return (
+            <div className="item-container" key={item.id}>
+              <h4>{item.name}</h4>
+              <h5>{item.alterEgo}</h5>
+            </div>
+          );
+        })}
+      </section>
+    </>
+  );
 };
